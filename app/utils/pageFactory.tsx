74f2def16cdaac6a28cedb7ef1c81f7077c2b9ae
Saheed Lasisi <colosian@yahoo.com>
2025-03-05 13:51:23 +0000

    removed utils folder

--- app/utils/pageFactory.tsx
+++
@@ -1,9 +0,0 @@
-// app/pages/discover/components/page.tsx
-import { APIComponentsPage } from '@/components/utils/pageFactory';
-
-export default APIComponentsPage;
-
-// app/pages/observe/data-quality/page.tsx
-import { DataQualityPage } from '@/components/utils/pageFactory';
-
-export default DataQualityPage;
